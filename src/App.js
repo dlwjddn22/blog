a1d369ffe0e87d44321868ec186fd894b518eb3a
ljw <dlwjddn22@nate.com>
2022-11-04 00:17:03 +0900

    first commit reactStudy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let dataVal = "123";
+  
+  function test()
+  {
+    return 100;
+  }
+  
+  return (
+    <div className="App">
+      <div className='black-nav'>
+        <div> 개발 Blog </div>
+      </div>
+      <img src={ logo } />
+      <h4 style={ { color:'blue', fontSize : '30px' } }>{ dataVal }</h4>
+      <h4>{ test() }</h4>
+    </div>
+  );
+}
+
+export default App;
